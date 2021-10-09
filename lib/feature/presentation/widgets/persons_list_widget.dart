effdd825a6f9c36c152c9c417cac2eea7ff25df6
Igor` <webarts@yandex.ru>
2021-10-09 11:16:43 +0800

    Подгрузка новых персонажей

--- lib/feature/presentation/widgets/persons_list_widget.dart
+++ lib/feature/presentation/widgets/persons_list_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_rick_morty_app/feature/domain/entities/person_entity.dart';
@@ -6,16 +8,38 @@ import 'package:flutter_rick_morty_app/feature/presentation/bloc/person_list_cub
 import 'package:flutter_rick_morty_app/feature/presentation/widgets/person_card_widget.dart';
 
 class PersonsList extends StatelessWidget {
-  const PersonsList({Key key}) : super(key: key);
+  final scrollController = ScrollController();
+
+  void setupScrollController(BuildContext context) {
+    scrollController.addListener(() {
+      if (scrollController.position.atEdge) {
+        if (scrollController.position.pixels != 0) {
+          //BlocProvider.of<PersonListCubit>(context).loadPerson();
+          context.read<PersonListCubit>().loadPerson();
+        }
+      }
+    });
+  }
+
+  PersonsList({
+    Key key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    setupScrollController(context);
+
     return BlocBuilder<PersonListCubit, PersonState>(
       builder: (context, state) {
         List<PersonEntity> persons = [];
 
+        bool isLoading = false;
+
         if (state is PersonLoading && state.isFirstFetch) {
           return _loadingIndicator();
+        } else if (state is PersonLoading) {
+          persons = state.oldPersonsList;
+          isLoading = true;
         } else if (state is PersonLoaded) {
           persons = state.personsList;
         } else if (state is PersonError) {
@@ -25,15 +49,25 @@ class PersonsList extends StatelessWidget {
         return Padding(
           padding: const EdgeInsets.symmetric(vertical: 16.0),
           child: ListView.separated(
+            controller: scrollController,
             itemBuilder: (context, index) {
-              return PersonCard(person: persons[index]);
+              if (index < persons.length) {
+                return PersonCard(person: persons[index]);
+              } else {
+                Timer(Duration(milliseconds: 500), () {
+                  scrollController.jumpTo(
+                    scrollController.position.maxScrollExtent,
+                  );
+                });
+                return _loadingIndicator();
+              }
             },
             separatorBuilder: (context, index) {
               return Divider(
                 color: Colors.grey[400],
               );
             },
-            itemCount: persons.length,
+            itemCount: persons.length + (isLoading ? 1 : 0),
           ),
         );
       },
